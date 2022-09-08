e942efe45320a64445bead300347a63d90563011
You Song Hou <yousonghou@gmail.com>
2022-09-07 22:08:09 -0400

    feat: update hardhat.config.js to deploy a contract on Skale Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network skale
+    skale: {
+      url: "https://eth-online.skalenodes.com/v1/hackathon-complex-easy-naos",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 647426021,
+      gasPrice: 8000000000
+    }
+  },
 };
