5e435b201a9ff108272aa9e0d579321f9d023c99
You Song Hou <yousonghou@gmail.com>
2022-09-14 00:22:44 -0400

    feat: update hardhat.config.js to deploy a contract on Aurora Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,18 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    // npx hardhat run scripts/deploy.js --network skale
-    skale: {
+    // npx hardhat run scripts/deploy.js --network skaletest
+    skaletest: {
       url: "https://eth-online.skalenodes.com/v1/hackathon-complex-easy-naos",
       accounts: [process.env.PRIVATEKEY],
       chainId: 647426021,
+      gasPrice: 8000000000
+    },
+    // npx hardhat run scripts/deploy.js --network auroratest
+    auroratest: {
+      url: "https://testnet.aurora.dev/",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 1313161555,
       gasPrice: 8000000000
     }
   },
